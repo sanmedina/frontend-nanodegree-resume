0112e4149f7728963f0f86cb26c6264eb03bd2b4
Santiago Lizarazu <sanlizmed@gmail.com>
2015-10-06 23:33:14 -0500

    Until leson 1 completed

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,35 +1,64 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//var s1 = "jamon queso";
-//var s2 = s1.replace("queso", "carne");
-//$("#main").append(s1);
-//$("#main").append(s2);
-//console.log(s1);
-//console.log(s2);
 var name = "Dev Medina";
 var role = "Developer";
-var formattedName = HTMLheaderName.replace("%data%",name);
-var formattedRole = HTMLheaderRole.replace("%data%",role);
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
 
-var bio = {
-    "name" : "Dev Medina",
-    "role" : "Developer",
-    "contactInfo" : { 
-        mail : "devmedina@mail.com",
-        telephone : "44444444"
-    },
-    "pictureURL" : "images/fry.jpg",
-    "welcomeMessage" : "¡Hola mundo!",
-    "skills" : [
-        "programming", "eating", "sleeping"
-    ]
+//$("#main").append("<br>");
+
+var work = {
+    "title" : "",
+    "employer" : "",
+    "location" : "",
+    "yearsWorked" : 0,
+    "description" : "",
+    "projectImages" : []
 };
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contactInfo);
-$("#main").append(bio.pictureURL);
-$("#main").append(bio.welcomeMessage);
-$("#main").append(bio.skills);
+
+var works = {
+    "works" : []
+};
+
+var project = {
+    "title" : "",
+    "monthsWorked" : 0,
+    "description" : "",
+    "projectImages" : []
+};
+
+var projects = {
+    "projects" : []
+};
+
+var bio = {
+    "name" : "",
+    "role" : "",
+    "welcomeMessage" : "",
+    "contacts" : [],
+    "skills" : []
+};
+
+var school = {
+    "name" : "",
+    "location" : "",
+    "degree" : "",
+    "majors" : [],
+    "yearsAttended" : 0,
+    "website" : ""
+};
+
+var onlineCourse = {
+    "title" : "",
+    "school" : "",
+    "datesAtended" : 0,
+    "courseURL" : ""
+};
+
+var education = {
+    "schools" : [],
+    "onlineCourses" : []
+};
