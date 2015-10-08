8eeaec6307b5cdacc4cbd6aa3b1975289610154e
Santiago Lizarazu <sanlizmed@gmail.com>
2015-10-07 21:30:29 -0500

    Course completed without finishing the project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,54 +11,145 @@ $("#header").prepend(formattedRole);
 //$("#main").append("<br>");
 
 var work = {
-    "title" : "",
-    "employer" : "",
-    "location" : "",
-    "yearsWorked" : 0,
-    "description" : "",
-    "projectImages" : []
-};
-
-var works = {
-    "works" : []
-};
-
-var project = {
-    "title" : "",
-    "monthsWorked" : 0,
-    "description" : "",
-    "projectImages" : []
+    "jobs" : [
+        {
+            "title" : "WinDev Developer",
+            "employer" : "EA-strategy",
+            "location" : "Bogotá, D.C.",
+            "yearsWorked" : 0.7,
+            "description" : "Doing everithing",
+            "projectImages" : []
+        }
+    ]
 };
 
 var projects = {
-    "projects" : []
+    "projectsW" : [
+        {
+            "title" : "Trazabilidad",
+            "monthsWorked" : 3,
+            "description" : "Work controller for a clinic laboratory.",
+            "projectImages" : []
+        },
+        {
+            "title" : "MailRoom",
+            "monthsWorked" : 2,
+            "description" : "Mail control version.",
+            "projectImages" : []
+        }
+    ],
+    "display" : function() {
+        for( projectIndex in this.projectsW )
+        {
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", this.projectsW[projectIndex].title);
+            var formattedProjectMonthsWorked = HTMLprojectDates.replace("%data%", this.projectsW[projectIndex].monthsWorked);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", this.projectsW[projectIndex].description);
+            
+            $("#projects").append(HTMLprojectStart);
+            $(".project-entry:last").append(formattedProjectTitle);
+            $(".project-entry:last").append(formattedProjectMonthsWorked);
+            $(".project-entry:last").append(formattedProjectDescription);
+        }
+    }
 };
 
 var bio = {
-    "name" : "",
-    "role" : "",
-    "welcomeMessage" : "",
-    "contacts" : [],
-    "skills" : []
-};
-
-var school = {
-    "name" : "",
-    "location" : "",
-    "degree" : "",
-    "majors" : [],
-    "yearsAttended" : 0,
-    "website" : ""
-};
-
-var onlineCourse = {
-    "title" : "",
-    "school" : "",
-    "datesAtended" : 0,
-    "courseURL" : ""
+    "name" : "Dev Medina",
+    "role" : "Developer",
+    "welcomeMessage" : "¡Hola mundo!",
+    "contacts" : [
+        {
+            "mail" : "devsanlizmed@yahoo.com"
+        }
+    ],
+    "skills" : [
+        "patience",
+        "sleeping",
+        "gaming",
+        "eating"
+    ]
 };
 
 var education = {
-    "schools" : [],
-    "onlineCourses" : []
+    "schools" : [
+        {
+            "name" : "Javeriana",
+            "location" : "Bogotá, D.C.",
+            "degree" : "Bachelor",
+            "majors" : [ "SE" ],
+            "yearsAttended" : 6,
+            "website" : "http://www.javeriana.edu.co"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "Intro to HTML and CSS",
+            "school" : "UDacity",
+            "datesAtended" : 5,
+            "courseURL" : "http://www.udacity.com/"
+        },
+        {
+            "title" : "JavaScript Basics",
+            "school" : "UDacity",
+            "datesAtended" : 5,
+            "courseURL" : "http://www.udacity.com/"
+        }
+    ]
 };
+
+if (bio.skills.length > 0) {
+    var formattedSkills;
+    var skillIndex;
+    
+    $("#header").append(HTMLskillsStart);
+    for (skillIndex in bio.skills) {
+        formattedSkills = HTMLskills.replace("%data%", bio.skills[skillIndex]);
+        $("#skills").append(formattedSkills);
+    }
+}
+
+function displayWork() {
+    var jobIndex;
+    var formattedWorkEmployer;
+    var formattedWorkTitle;
+    var formattedWorkLocation;
+    var formattedWorkDates;
+    var formattedWorkDescription;
+    
+    for (jobIndex in work.jobs) {
+        formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIndex].employer);
+        formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIndex].title);
+        formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[jobIndex].location);
+        formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[jobIndex].yearsWorked);
+        formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[jobIndex].description);
+        
+        $("#workExperience").append(HTMLworkStart);
+        $(".work-entry:last").append(formattedWorkEmployer + formattedWorkTitle);
+        $(".work-entry:last").append(formattedWorkLocation);
+        $(".work-entry:last").append(formattedWorkDates);
+        $(".work-entry:last").append(formattedWorkDescription);
+    }
+}
+displayWork();
+$(document).click(function (loc) {
+    "use strict";
+    var x = loc.pageX, y = loc.pageY;
+    
+    logClicks(x, y);
+});
+
+function inName(name) {
+    var spacePos = name.indexOf(" ");
+    var upperChar = name.slice(0, 1);
+    var firstName = name.slice(1, spacePos);
+    var lastName = name.slice(spacePos + 1, name.length);
+    
+    upperChar = upperChar.toUpperCase();
+    firstName = firstName.toLowerCase();
+    lastName = lastName.toUpperCase();
+    
+    return upperChar + firstName + " " + lastName;
+}
+
+projects.display();
+$("#mapDic").append(googleMap);
